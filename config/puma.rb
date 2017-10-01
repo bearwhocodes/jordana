c6695c0cc1f1511a37b22417bc190b9e8e073bbe
Steve Fenn <steve@bearwho.codes>
2017-10-01 20:53:50 +0100

    Removing dotenv from procfile

--- config/puma.rb
+++ config/puma.rb
@@ -1,11 +1,3 @@
-# Load dot env first
-begin
-  require "dotenv"
-  Dotenv.load("/.env")
-rescue
-  # Not in development
-end
-
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
