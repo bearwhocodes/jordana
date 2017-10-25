c3958cb5c54abd4dc9be1022483e2dbe1b691047
Steve Fenn <steve@bearwho.codes>
2017-10-25 17:00:06 +0100

    Changes for deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,5 @@
 server '139.59.160.219', user: 'deploy', roles: %w{web app db}
 
-set :rvm_ruby_version, '2.4.1'
 set :branch, 'master'
 set :rails_env, 'production'
-set :deploy_to '/var/www/jordana/www'
+set :deploy_to, '/var/www/jordana/www'
